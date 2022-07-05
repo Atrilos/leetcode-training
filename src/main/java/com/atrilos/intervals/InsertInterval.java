49b4d8b8b76b706152f006774e7707c5923ceaf1
Atrilos <101889118+Atrilos@users.noreply.github.com>
2022-07-05 13:46:01 +0700

    day 11

---
+++ src/main/java/com/atrilos/intervals/InsertInterval.java
@@ -0,0 +1,74 @@
+package com.atrilos.intervals;
+
+import java.util.*;
+
+/**
+ * You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi]
+ * represent the start and the end of the ith interval and intervals is sorted in ascending order by starti.
+ * You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+ *
+ * Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals
+ * still does not have any overlapping intervals (merge overlapping intervals if necessary).
+ *
+ * Return intervals after the insertion.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * Output: [[1,5],[6,9]]
+ * Example 2:
+ *
+ * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * Output: [[1,2],[3,10],[12,16]]
+ * Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+ *
+ *
+ * Constraints:
+ *
+ * 0 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^5
+ * intervals is sorted by starti in ascending order.
+ * newInterval.length == 2
+ * 0 <= start <= end <= 10^5
+ */
+public class InsertInterval {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(new InsertInterval().insert(new int[][]{{1, 5}}, new int[]{0, 3})));
+    }
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        //[ ] [ ]
+        //        [newInterval]
+        //find all intervals with its end before newInterval.start
+        while(i < intervals.length && intervals[i][1] < newInterval[0]){
+            result.add(intervals[i]);
+            i++;
+        }
+
+        //merge all intervals that overlap with `newInterval` -> its start is before newInterval.end
+        //  [ ] [ ] [ ]  [ ]
+        //   [newInterval]
+        while(i < intervals.length && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        //add the `newInterval`
+        result.add(newInterval);
+
+        //add the rest intervals that is after `newInterval`
+        //                 [ ] [ ] [ ] [ ]
+        //   [newInterval]
+        while(i < intervals.length){
+            result.add(intervals[i]);
+            i++;
+        }
+        // convert to int[][] array
+        return result.toArray(new int[result.size()][]);
+    }
+}
